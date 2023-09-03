61d5f925a3b031ed401507bc58f4faa3aad9451e
JagateesMac <140966272+Jagatees@users.noreply.github.com>
2023-09-03 16:31:51 +0800

    void push

--- files/project/project.js
+++ files/project/project.js
@@ -1,11 +1,19 @@
-function filterCards(tag) {
-    const cards = document.querySelectorAll('.card');
-    cards.forEach(card => {
-      const tags = card.getAttribute('data-tags').split(' ');
-      if (tags.includes(tag) || tag === 'all') {
-        card.style.display = 'block';
-      } else {
-        card.style.display = 'none';
-      }
+// JavaScript to handle filter tab functionality
+const filterTabs = document.querySelectorAll('.filter-tab');
+const filterCards = document.querySelectorAll('.filter-card');
+
+filterTabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const category = tab.getAttribute('data-category');
+        filterTabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+
+        filterCards.forEach(card => {
+            if (category === 'all' || card.getAttribute('data-category') === category) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
     });
-  }
+});
